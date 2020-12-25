e52b7682eee0b08f5f535e13471594f32ecc15ec
GurraT <gustafthomsson@hotmail.com>
2020-12-25 13:27:35 +0000

    FIxed open/close function accordion, removed junc-code, added image to neighbourhoodpages, adjusted colortheme

--- function.js
+++
@@ -1,8 +0,0 @@
-const items = document.querySelectorAll(".accordion h3");
- 
-function toggleAccordion(){
-  this.classList.toggle('active');
-  this.nextElementSibling.classList.toggle('active');
-}
- 
-items.forEach(item => item.addEventListener('click', toggleAccordion));
